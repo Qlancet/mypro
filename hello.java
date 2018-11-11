625c4a2bf97ec84946e3a38f0d33ccd81f4d5d49
Qlancet <z464337866@163.com>
2018-11-12 00:13:36 +0800

    hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ package com.itheima.test;
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello henan!");
+		System.out.println("hello hebei!");
 		System.out.println("hello world!");
 	}
 }
