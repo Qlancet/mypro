d2072ed229e281f6dd8147b3c478f68fd3238aac
Qlancet <z464337866@163.com>
2018-11-12 00:09:39 +0800

    修改两个文件

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ package com.itheima.test;
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello china!");
+		System.out.println("hello henan!");
 		System.out.println("hello world!");
 	}
 }
