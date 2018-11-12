b88c2b7c5520867bc80eee1398f5df327406a40e
Qlancet <z464337866@163.com>
2018-11-13 00:48:28 +0800

    brh modify

--- hello.java
+++ hello.java
@@ -5,6 +5,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("hello hebei!");
 		System.out.println("hello world!");
-		System.out.println("hello!");
+		System.out.println("hello  brh!");
 	}
 }
