e4999e7651af97b5391d88b9339f87de41193afa
Qlancet <z464337866@163.com>
2018-11-13 18:50:22 +0800

    add  static

--- emp.java
+++ emp.java
@@ -1,7 +1,7 @@
 package com.itheima.test;
 
 public class hello {
-
+	public  static  final String COMPANY_NAME="yook";
 	public static void main(String[] args) {
 		private String usernam;
 		private int age;
