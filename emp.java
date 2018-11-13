7f17c68100ce2a36b04a9985ddbe52e9eece9383
Qlancet <z464337866@163.com>
2018-11-13 18:54:53 +0800

    add main method

---
+++ emp.java
@@ -0,0 +1,13 @@
+package com.itheima.test;
+
+public class hello {
+	
+	public static void main(String[] args) {
+		private String usernam;
+		private int age;
+		private String detail;
+	}
+	public static void main(String[] args) {
+		System.out.println("*    hello world!")
+	}
+}
