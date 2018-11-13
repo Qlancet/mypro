25f06ebafcc999ec06a20834530ac8b2ab8aaf27
Qlancet <z464337866@163.com>
2018-11-13 23:36:59 +0800

    patch apply

--- emp.java
+++ emp.java
@@ -1,20 +1,15 @@
 package com.itheima.test;
 
 public class hello {
-<<<<<<< HEAD
-	
-=======
 	public  static  final String COMPANY_NAME="yook";
->>>>>>> brh
 	public static void main(String[] args) {
 		private String usernam;
 		private int age;
 		private String detail;
+		private  int  age;
+		private  String demo;
 	}
-<<<<<<< HEAD
 	public static void main(String[] args) {
 		System.out.println("*    hello world!")
 	}
-=======
->>>>>>> brh
 }
